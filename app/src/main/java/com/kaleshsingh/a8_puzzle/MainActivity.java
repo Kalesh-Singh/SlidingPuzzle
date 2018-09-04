6b542c34ffec8003e492cb7b6b275645ef26721a
Kalesh <kaleshsingh96@gmail.com>
2018-09-03 20:30:42 -0700

    Removed debug logging

--- app/src/main/java/com/kaleshsingh/a8_puzzle/MainActivity.java
+++ app/src/main/java/com/kaleshsingh/a8_puzzle/MainActivity.java
@@ -5,7 +5,6 @@ import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -35,8 +34,6 @@ public class MainActivity extends AppCompatActivity {
                     savedInstanceState.getIntArray(Puzzle.GAME_STATE),
                     savedInstanceState.getInt(Puzzle.EMPTY_TILE));
         }
-
-        Log.d("Lifecycle", "onCreate");
     }
 
     @Override
